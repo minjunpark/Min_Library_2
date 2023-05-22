b229a83502335675757b161515c87cca70d92add
MinJun Park <zoshs38@gmail.com>
2023-05-23 04:12:02 +0900

    malloc변경버전

--- MemoryPoll/CMemoryPool.h
+++ MemoryPoll/CMemoryPool.h
@@ -30,10 +30,13 @@ private:
 	int m_iCapacity;//메모리풀 전체 개수
 	int m_iUseCount;//사용중인 사이즈
 	bool m_iPlacementNew;// Alloc 시 생성자 / Free 시 파괴자 호출 여부
+	int m_buffer_pointer;//생성될때마다 세팅되는 고유값
 
 	struct Node
 	{
+		int OVER_GUARD = 0;
 		DATA data;
+		int UNDER_GUARD = 0;
 		Node* next = nullptr;
 	};
 
@@ -53,16 +56,20 @@ public:
 		m_iCapacity = 0;//전체 총개수
 		m_iUseCount = 0;//사용 중인 개수
 		m_iPlacementNew = bPlacementNew;//Alloc 시 생성자 / Free 시 파괴자 호출 여부
-		//_FreeNode = nullptr;
+		m_buffer_pointer = (int)this;//생성될때 세팅되는 this포인터를 고유값으로 한다.
+
 		if (iBlockNum == 0)//0이라면 더 진행할 이유가 없음
 			return;
 
 		for (int iMemory = 0; iMemory < iBlockNum; iMemory++)//들어온횟수만큼 돌면서 노드생성
 		{
-			Node* NewNode = new Node;
+			Node* NewNode = (Node*)malloc(sizeof(Node));//생성자를 호출하지 않기위해 malloc으로 세팅
 
-			if (m_iPlacementNew == false)//Alloc시 생성자 파괴자를 호출하지 않을거라면
-				DATA* data = new(&(NewNode->data)) DATA;//미리 데이터 생성을해둔다.
+			if (m_iPlacementNew == false) //Alloc시 생성자 파괴자를 호출하지 않을거라면
+				DATA* data = new(&(NewNode->data)) DATA;//생성자를 미리 세팅해둔다.
+
+			NewNode->OVER_GUARD = m_buffer_pointer;//고유 포인터값을 노드에 세팅한다.
+			NewNode->UNDER_GUARD = m_buffer_pointer;//고유 포인터값을 노드에 세팅한다.
 
 			NewNode->next = _FreeNode;//스택
 			_FreeNode = NewNode;//쌓기
@@ -76,14 +83,16 @@ public:
 		if (m_iCapacity == 0)//0개라면 모두제거 된것이므로 소멸자를 더 진행할 필요가 없다.
 			return;
 
-		//모든 노드 제거
 		Node* _Node = _FreeNode;
-		while (_Node != nullptr)//다음 노드가 nullptr라면 모든노드가 비었다는 뜻이다.정지
+		while (_Node != nullptr)//자기자신이 _FreeNode노드가 된다면 모든 노드가 제거된것이다.
 		{
-			Node* tmpNode = _Node;//노드 임시로 세팅하고
-			delete tmpNode;//노드제거
+			Node* tmpNode = _Node;//노드 임시세팅
+			if (m_iPlacementNew == false)//소멸자를 Free에서 호출하지 않게 만들었기때문에
+				tmpNode->data.~DATA();//객체의 소멸자를 호출시키면서
+			free(tmpNode);//사용하는 실제 메모리를 free상태로 바꿔준다.
 			_Node = _Node->next;//다음노드로 넘어간다.
 		}
+
 	};
 
 	//////////////////////////////////////////////////////////////////////////
@@ -100,45 +109,33 @@ public:
 			DATA* Data;
 
 			if (m_iPlacementNew == true)//Alloc시점에 생성자를 호출해주길 바라는상태
-				Data = new (&(OldNode->data)) DATA;//data포인터 자체를 palcement_new로 위치로잡아서 메모리처리한다.
+				Data = new (&(OldNode->data)) DATA;//data포인터 자체를 palcement_new로 위치로잡아서 생성해준다.
 			else
 				Data = &(OldNode->data);//이미 데이터 생성자가 세팅된 경우기 때문에 스택에서 뽑은값 바로리턴
-
-			//1. top을 top의 link로 할당.
-			//2. top 앞의 메모리 해제
-			//OldNode->next = _FreeNode;
-			//_FreeNode = OldNode;
-
-			//OldNode = OldNode->next;//노드상에서 제거-> 밖으로 데이터를 던진다.
-			//_FreeNode = OldNode;//노드를 지웠으니 프리노드도 바꿔준다.
-
-			//OldNode->next = _FreeNode;
-			//_FreeNode = OldNode;
 
 			_FreeNode = OldNode->next;//스택에서 노드 제거
 
 			m_iUseCount++;//총사용량을 올려준다.
-			//return &OldNode->data;
-			return Data;
+
+			return Data;//할당된 메모리를 리턴해준다.
 		}
 		else//사용 할 수 있는 메모리 오브젝트가 없다면 만들어서 리턴해준다.
 		{
-			Node* NewNode = new Node;
+			Node* NewNode = (Node*)malloc(sizeof(Node));//생성자를 호출하지 않기위해 malloc으로 세팅
+			
+			DATA* Data = new (&(NewNode->data)) DATA;//생성자를 호출하면서 데이터 세팅
 
-			DATA* Data = new (&(NewNode->data)) DATA;
+			NewNode->OVER_GUARD = m_buffer_pointer;
+			NewNode->UNDER_GUARD = m_buffer_pointer;
 
 			NewNode->next = _FreeNode;//스택에 노드 추가
 			_FreeNode = NewNode;
-
-			//NewNode = NewNode->next;
-			//_FreeNode = NewNode;
 
 			_FreeNode = NewNode->next;//스택에서 노드 제거
 
 			m_iUseCount++;//사용량 올려주고
 			m_iCapacity++;//노드 총 개수를 증가시켜준다.
 
-			//return &NewNode->data;
 			return Data;
 		}
 	};
@@ -162,10 +159,16 @@ public:
 
 		if (m_iUseCount > 0)//현재 사용중인 값이 있다면
 		{
-			Node* ReData = (Node*)pData;
+			//Alloc시 준값은 Node 가아니라 Node안의 data부분이므로 Node가장위부터 data까지를 계산한다.
+			//그후 계산한 값을 들어온 위치에서 타고 올라가 Node 위치를 정확히 세팅한다.
+			Node* ReData = (Node*)((char*)pData - offsetof(Node, data));
+
+			if (ReData->OVER_GUARD != m_buffer_pointer || ReData->UNDER_GUARD != m_buffer_pointer)
+				return false;//다른 오브젝트의 포인터가 들어왔거나 데이터가 오염됬다는 뜻이다.
+			//로그 남기거나 exception 일으키기가 필요한데 우선 false만 처리한다.
 
 			if (m_iPlacementNew == true)//소멸자를 호출해야하는 상황이라면
-				ReData->data.~DATA();
+				ReData->data.~DATA();//소멸자를 호출해준다.
 
 			ReData->next = _FreeNode;//돌아온 메모리를
 			_FreeNode = ReData;//스택에 다시 쌓는다.
--- MemoryPoll/main.cpp
+++ MemoryPoll/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include "CMemoryPool.h"
 
+class st_test
+{
+	
+public:
+	int hp;
+	int x;
+	int y;
+	st_test() 
+	{
+		hp = 0;
+		x = 0;
+		y = 0;
+		printf("st_test\n");
+	}
+
+	virtual ~st_test() 
+	{
+		printf("~st_test\n");
+	}
+};
+
 struct st_Player
 {
 	int hp;
@@ -8,7 +29,14 @@ struct st_Player
 	int y;
 };
 
-
+struct st_test2
+{
+	int a;
+	int b;
+	char c;
+	int k;
+	char d;
+};
 
 struct st_Monster
 {
@@ -19,6 +47,10 @@ struct st_Monster
 
 int main()
 {
+	//st_test2 a;
+
+	//printf("%d",offsetof(st_test2, d));
+
 	//Node_text* FreeNode = new Node_text;
 
 	////Node_text* n1;
@@ -36,12 +68,31 @@ int main()
 
 	//data->~st_Player();//소멸자를 호출해서 데이터 제거
 
-	CMemoryPool<st_Player> Pool(0, false);
-	
+	//CMemoryPool<st_Player> Pool(0, false);
+	CMemoryPool<st_test> Pool(0, false);
+	//CMemoryPool<st_Player> Pool2(5, false);
+
+	//printf("%d",1);
+	//CMemoryPool<st_test> Pool5(0, true);
+	//st_test* p1 = Pool5.Alloc();
+	//st_test* p2 = Pool5.Alloc();
+	//st_test* p3 = Pool5.Alloc();
+	//st_test* p4 = Pool5.Alloc();
+	//st_test* p5 = Pool5.Alloc();
+
+	//Pool5.Free(p1);
+	//Pool5.Free(p2);
+	//Pool5.Free(p3);
+	//Pool5.Free(p4);
+	//Pool5.Free(p5);
+
 	//Pool(300, false);
-	st_Player* p = Pool.Alloc();//할당
-	st_Player* k = Pool.Alloc();
-	st_Player* q = nullptr;
+	
+	st_test* p = Pool.Alloc();//할당
+	st_test* k = Pool.Alloc();
+	//st_Player* v = Pool2.Alloc();
+	//Pool.Free(k);
+	//Pool.Free(v);
 
 	p->hp = 1;
 	p->x = 2;
@@ -52,33 +103,39 @@ int main()
 	printf("%d %d %d\n", p->hp, p->x, p->y);
 	printf("%d %d %d\n", k->hp, k->x, k->y);
 	Pool.print();
-	st_Player* s = Pool.Alloc();;
+	st_test* s = Pool.Alloc();;
 	s->hp = 7;
 	s->x = 8;
 	s->y = 9;
 	Pool.Free(s);
 	Pool.Free(k);
 	Pool.Free(p);
-	printf(" &(NewNode->data) %p\n", p);
-	printf("data %p\n", k);
+	//printf(" &(NewNode->data) %p\n", p);
+	//printf("data %p\n", k);
 	printf("p : %d %d %d\n", p->hp, p->x, p->y);
 	printf("k : %d %d %d\n", k->hp, k->x, k->y);
 	printf("s : %d %d %d\n", s->hp, s->x, s->y);
-	st_Player* l = Pool.Alloc();;
-	st_Player* w = Pool.Alloc();;
+	st_test* l = Pool.Alloc();;
+	st_test* w = Pool.Alloc();;
+	st_test* g = Pool.Alloc();;
+	st_test* m = Pool.Alloc();;
 	printf("l : %d %d %d\n", l->hp, l->x, l->y);
 	printf("w : %d %d %d\n", w->hp, w->x, w->y);
+	printf("g : % d % d % d\n", g->hp, g->x, g->y);
+	printf("m : % d % d % d\n", m->hp, m->x,m->y);
 	Pool.Free(l);
 	Pool.Free(w);
+	Pool.Free(g);
+	Pool.Free(m);
 	Pool.print();
-	//p->hp = 1;
-	//p->x = 2;
-	//p->y = 3;
-	//Pool.print();
+	p->hp = 1;
+	p->x = 2;
+	p->y = 3;
+	Pool.print();
 	//printf("%d\n",Pool.Free(k));
 	//printf("%d\n", Pool.Free(k));
-	////printf("%d\n", Pool.Free(p));
-	////printf("%d %d %d\n", p->hp, p->x, p->y);
-	////Pool.Free(p);//초기화
+	//printf("%d\n", Pool.Free(p));
+	//printf("%d %d %d\n", p->hp, p->x, p->y);
+	//Pool.Free(p);//초기화
 	return 0;
 }
